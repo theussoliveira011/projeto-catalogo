ca776dbba63b19d3caf832ac9219bd68dea0fd95
Matheus Oliveira <106394398+theussoliveira011@users.noreply.github.com>
2022-06-27 18:26:16 -0300

    car search

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var searchCars = function(cars, searchName){
     input = searchName.toLowerCase();
     
-    for (var i = 0; i < cars.length; i++) {
+    for (var i = 0; i < cars.length; i++) { // Estrutura de repetição que passa por todo elemento para garantir a exatidão da comparação.
         if (!cars[i].textContent.toLowerCase().includes(input)) {
-            cars[i].parentElement.parentElement.style.display="none";
+            cars[i].parentElement.parentElement.style.display="none"; /*Caso o texto digitado não seja correspondente a constastante "cars", não é executado nada*/
         }else {
-            cars[i].parentElement.parentElement.style.display="flex";                 
+            cars[i].parentElement.parentElement.style.display="flex"; /*Caso o elemento seja igual ele atualiza o display e mostra o carro*/                
         }
     };
 };
 
-const cars = document.querySelectorAll('.car-name');
-const searchElement = document.querySelector('.search-import');
+const cars = document.querySelectorAll('.car-name'); /* Criação de variavel da classe .car-name.*/
+const searchElement = document.querySelector('.search-import'); /* atribuição de valor a classe .search-import*/
 
-searchElement.addEventListener('change', (event) => searchCars(cars, searchElement.value));
+searchElement.addEventListener('change', (event) => searchCars(cars, searchElement.value)); // O evento chama a função.
